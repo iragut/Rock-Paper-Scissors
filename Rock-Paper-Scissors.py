cb156c609d075bbdabacf269015b3c9910136aaf
iragut <74363823+iragut@users.noreply.github.com>
2022-09-07 23:38:08 +0300

    Implemet the game

--- Rock-Paper-Scissors.py
+++ Rock-Paper-Scissors.py
@@ -1,7 +1,22 @@
+import random
+
+game_words = ["rock", "scissors", "paper"]
+calculator_move = random.choice(game_words)
+
 player_move = input()
-if player_move == "rock":
-    print("Sorry, but the computer chose paper")
-elif player_move == "scissors":
+if player_move == calculator_move:
+    print(f"There is a draw ({player_move})")
+elif player_move == "scissors" and calculator_move == "rock":
     print("Sorry, but the computer chose rock")
-else:
+elif player_move == "scissors" and calculator_move == "paper":
+    print("Well done. The computer chose paper and failed")
+
+elif player_move == "rock" and calculator_move == "paper":
+    print("Sorry, but the computer chose paper")
+elif player_move == "rock" and calculator_move == "scissors":
+    print("Well done. The computer chose scissors and failed")
+
+elif player_move == "paper" and calculator_move == "scissors":
     print("Sorry, but the computer chose scissors")
+elif player_move == "paper" and calculator_move == "rock":
+    print("Well done. The computer chose rock and failed")
