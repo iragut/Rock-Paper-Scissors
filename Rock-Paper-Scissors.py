36db47f51b3ac8173e8c69e0bde6e8349800ffa9
iragut <74363823+iragut@users.noreply.github.com>
2022-09-07 23:09:30 +0300

    Add the initial move

---
+++ Rock-Paper-Scissors.py
@@ -0,0 +1,7 @@
+player_move = input()
+if player_move == "rock":
+    print("Sorry, but the computer chose paper")
+elif player_move == "scissors":
+    print("Sorry, but the computer chose rock")
+else:
+    print("Sorry, but the computer chose scissors")
