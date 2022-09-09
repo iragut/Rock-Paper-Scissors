a43f6e3acd2ad2c17018ba6bedacfcd890af0cd3
iragut <74363823+iragut@users.noreply.github.com>
2022-09-09 21:51:26 +0300

    add the possibility to add more choices

--- Rock-Paper-Scissors.py
+++ Rock-Paper-Scissors.py
@@ -1,6 +1,7 @@
 import random
 
-game_words = ["rock", "scissors", "paper"]
+game_words = ["rock", "paper", "scissors"]
+
 
 dict_names = {}
 with open("rating.txt") as f:
@@ -8,43 +9,43 @@ with open("rating.txt") as f:
         (key, val) = line.split()
         dict_names[key] = val
 
-name = input("Enter your name:")
+name = input("Enter your name:")        # take the name for rating if name in file take from there
 print(f"Hello, {name}")
 if name in dict_names:
     rating = int(dict_names[name])
 else:
     rating = 0
 
+winning_list = None
+more_words = input().split(',')
+
+if more_words != ['']:
+    game_words = more_words
+
+print("Okay, let's start")
 
 while True:
-    calculator_move = random.choice(game_words)
-    player_move = input()
-    if player_move not in game_words and player_move != "!exit" and player_move != "!rating":
-        print("Invalid input")
-    elif player_move == "!exit":
+    player_choose = input()
+    if player_choose == '!exit':    # the game
         break
-    elif player_move == "!rating":
+    elif player_choose == '!rating':
         print(f"Your rating: {rating}")
-    else:
-        if player_move == calculator_move:
-            print(f"There is a draw ({player_move})")
+
+    elif player_choose not in game_words:
+        print("Invalid input")
+
+    elif player_choose in game_words:       # create the winning list
+        calculator_move = random.choice(game_words)
+        winning_list = game_words[game_words.index(player_choose) + 1:]
+        winning_list += game_words[:game_words.index(player_choose)]
+        winning_list = winning_list[:len(winning_list) // 2]
+
+        if calculator_move in winning_list:      # conclusion of the game
+            print(f"Sorry, but the computer chose {calculator_move}")
+        elif player_choose == calculator_move:
+            print(f"There is a draw ({player_choose})")
             rating += 50
-
-        elif player_move == "scissors" and calculator_move == "rock":
-            print("Sorry, but the computer chose rock")
-        elif player_move == "scissors" and calculator_move == "paper":
-            print("Well done. The computer chose paper and failed")
-            rating += 100
-
-        elif player_move == "rock" and calculator_move == "paper":
-            print("Sorry, but the computer chose paper")
-        elif player_move == "rock" and calculator_move == "scissors":
-            print("Well done. The computer chose scissors and failed")
-            rating += 100
-
-        elif player_move == "paper" and calculator_move == "scissors":
-            print("Sorry, but the computer chose scissors")
-        elif player_move == "paper" and calculator_move == "rock":
-            print("Well done. The computer chose rock and failed")
+        else:
+            print(f"Well done. The computer chose {calculator_move} and failed")
             rating += 100
 print("Bye!")
